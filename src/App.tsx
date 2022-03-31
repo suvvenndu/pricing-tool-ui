c2b5bf42bbb13e5081211b515d65a4164de2ae62
Subhendu Swain <suvendu.swain@gmail.com>
2022-03-31 11:00:30 +1100

    Display summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {getProposals, getProposalSummary} from './service/PricingToolService';
+import React, {useEffect} from 'react';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import  {Table} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    const [proposals, setProposals] = React.useState([{
+        proposalId: "",
+        proposalName: "",
+        customerGroup:"",
+        date: "",
+        description:"",
+        status:"",
+        summaryId:""
+    }]);
+    useEffect(() => {
+        getProposals().then(p => {
+            console.log(p.data);
+            setProposals(p.data);
+        });
+    }, []);
+
+    return (
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Propposal Name</th>
+                        <th>Customer Group</th>
+                        <th>Date (Last Saved)</th>
+                        <th>Description</th>
+                        <th>Status</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    proposals.map(p => {
+                        // return <li key={p.proposalId}>{p.proposalName}</li>
+                        return <tr key={p.proposalId}>
+                            <td>{p.proposalName}</td>
+                            <td>{p.customerGroup}</td>
+                            <td>{p.date}</td>
+                            <td>{p.description}</td>
+                            <td>{p.status}</td>
+                            <td><button id={p.summaryId}>View Summary</button></td>
+                        </tr>
+                    })
+                }
+                </tbody>
+               
+            </Table>
+        </>
+    );
 }
 
 export default App;
